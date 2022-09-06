f8ef1e24d4a6d3dbb85df6534d7d989d132d944c
Daxe9 <davidexie694@gmail.com>
2022-09-06 11:45:42 +0200

    added sfml_text class

---
+++ sfml_text.h
@@ -0,0 +1,21 @@
+class SFMLText : public sf::Text {
+public:
+    sf::Text text;
+public:
+    SFMLText(const sf::String &title,  const sf::Font &font , const float x, const float y) {
+        // load the font
+        text.setFont(font);
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::Black);
+        // the order: setString, setOrigin, setPosition, is important to make it work
+        text.setString(title);
+        text.setOrigin(
+                text.getLocalBounds().width / 2.0f,
+                text.getLocalBounds().height / 2.0f);
+        text.setPosition(x, y);
+    }
+
+    void reset_position(const float x, const float y) {
+        text.setPosition(x, y);
+    }
+};
