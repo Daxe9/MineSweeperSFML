35d8c593334ef6695be669a42c5392576a4d2773
Daxe9 <davidexie694@gmail.com>
2022-09-07 16:19:26 +0200

    changed font size implementation

--- sfml_text.h
+++ sfml_text.h
@@ -3,10 +3,10 @@ class SFMLText : public sf::Text {
 public:
     sf::Text text;
 public:
-    SFMLText(const sf::String &title,  const sf::Font &font , const float x, const float y) {
+    SFMLText(const sf::String &title, const sf::Font &font, int grid_size, const float x, const float y) {
         // load the font
         text.setFont(font);
-        text.setCharacterSize(24);
+        text.setCharacterSize(240 / grid_size);
         text.setFillColor(sf::Color::Black);
         // the order: setString, setOrigin, setPosition, is important to make it work
         text.setString(title);
